aa8dbd005febfc6e7ebfb249f6bc121a6d2e6e97
Christophe <cguegan@gmail.com>
2024-07-10 18:01:34 +0200

    Initial Commit

---
+++ SwiftuiAuthBase/SwiftuiAuthBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiAuthBaseApp.swift
+//  SwiftuiAuthBase
+//
+//  Created by Christophe Guégan on 10/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiAuthBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
